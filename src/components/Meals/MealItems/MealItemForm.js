884da1a1faef11f92983e24fd6202457fdb52489
Some1 <dushgowda@gmail.com>
2022-04-05 22:32:08 +0530

    input form

--- src/components/Meals/MealItems/MealItemForm.js
+++ src/components/Meals/MealItems/MealItemForm.js
@@ -1,5 +1,20 @@
-const MealItemForm = () => {
-    return (
+import classes from "./MealIemForm.module.css"
+import { Input } from "../../UI/Input";
+
+const MealItemForm = (props) => {
+    return(
+        <form className = { classes.form}>
+            <Input label = "Amount" input = {{
+                id: 'amount',
+                type: 'number',
+                min: '1',
+                max : '5',
+                step: '1' ,
+                defaultValue : '1'
+                }}
+            />
+            <button> + Add </button>
+        </form>
     );
 }
 
